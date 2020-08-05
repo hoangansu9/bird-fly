fe191096a514f587980fe07addb7b10859f2c04a
an <hoangansu9@gmail.com>
2020-08-05 12:39:40 +0700

    Updates

---
+++ precache-manifest.4682eeca90e464149a7a95e6ed2b925c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ba94980b597d085d2a22704e675a78",
+    "url": "/hoangansu9/bird-fly/index.html"
+  },
+  {
+    "revision": "bdf15a2d289b5c5aed30",
+    "url": "/hoangansu9/bird-fly/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe929dcb59c22452500e",
+    "url": "/hoangansu9/bird-fly/static/js/2.4f5d6b09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hoangansu9/bird-fly/static/js/2.4f5d6b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf15a2d289b5c5aed30",
+    "url": "/hoangansu9/bird-fly/static/js/main.1e7c327e.chunk.js"
+  },
+  {
+    "revision": "6e64ca2071e03fac7591",
+    "url": "/hoangansu9/bird-fly/static/js/runtime-main.dd4f4865.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hoangansu9/bird-fly/static/media/logo.5d5d9eef.svg"
+  }
+]);
